f97ba0911cbd30051526cc464ec3e74cff7bb427
Rifqi121 <rifqifauzanadziqra1@gmail.com>
2025-04-11 21:17:09 +0700

    add homepage

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:note/login.dart';
+import 'app.dart';
 
 void main() {
   runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: LoginPage(),
-    );
-  }
 }
